a770e0751a603806bb13b438783e7e3c309b6098
Karthiek Duggirala <karthiek390@gmail.com>
2025-06-14 21:26:33 -0400

    Remove sensitive environment var from the code.

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 WOLFRAM_API_URLS = {
     "full_results": "http://api.wolframalpha.com/v2/query",
     "short_answers": "http://api.wolframalpha.com/v1/result",
     "spoken_results": "http://api.wolframalpha.com/v1/spoken",
     "simple": "http://api.wolframalpha.com/v1/simple",
-    "llm": "https://www.wolframalpha.com/api/v1/llm-api",  # LLM API URL
+    "llm": "https://www.wolframalpha.com/api/v1/llm-api",
 }
-WOLFRAM_APPID = "KEHW67-AUJHW7355A"
+
+WOLFRAM_APPID = os.getenv("WOLFRAM_APPID")
